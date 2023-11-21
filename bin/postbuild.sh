68df8f73e8e372841aae7f5a17037bb35c5d863a
gentksb <gen@gensobunya.net>
2023-11-21 20:14:59 +0900

    トランスパイルをかませる

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -4,7 +4,7 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
 
-cp -r ./src ./.amplify-hosting/compute/default
+cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r public ./.amplify-hosting/static
