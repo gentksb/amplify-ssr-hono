7336fa69db6c59d901499ec9847e4ae6cdecd8c5
gentksb <gen@gensobunya.net>
2024-01-04 15:08:01 +0900

    fix

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -8,6 +8,6 @@ cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r public ./.amplify-hosting/static
-cp -r images ./.amplify-hosting/images
+cp -r images ./.amplify-hosting/compute/images
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
