1d8fedaff2391107d77e48c0a397e088d12ed917
gentksb <gen@gensobunya.net>
2024-01-04 17:54:57 +0900

    仕様を完全に理解した

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Hono } from "hono";
 import { html } from "hono/html";
 
 const app = new Hono();
-const imagePath = "/images/1600x1200.png";
+const imagePath = "/images?url=%2Fimages%2F1600x1200.png";
 
 const layout = html`
   <html lang="ja">
@@ -13,14 +13,14 @@ const layout = html`
       <title>Image Rendering Test</title>
     </head>
     <body>
-      <h1>Image Compare(1600x1200 origin or 800x600 view)</h1>
+      <h1>Image Compare(1600x1200 origin or w400 view)</h1>
       <p>
         ブラウザ開発者ツールからそれぞれの画像サイズを見るとサイズ圧縮されていることが分かります
       </p>
       <h2>Native HTML image</h3>
       <div>
         <img
-          src="${imagePath}"
+          src="${imagePath}&width=1600"
           alt="image-compare-1 native"
           width="400"
           height="300"
@@ -29,7 +29,7 @@ const layout = html`
       <h2>Parameter-based optimized image</h3>
       <div>
         <img
-          src="${imagePath}?width=400&height=300"
+          src="${imagePath}&width=400"
           alt="image-compare-2 optimized"
           width="400"
           height="300"
